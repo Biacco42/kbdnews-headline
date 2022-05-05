c0754911934e03e59058c6d9797a9830fe474974
Sano <sano@qoncept.co.jp>
2022-05-06 06:08:15 +0900

    Impl headline transition

--- kbdnews-headline.js
+++ kbdnews-headline.js
@@ -1,15 +1,18 @@
 'use strict';
 
 export function addHeadline() {
+    const headlinesContainer = document.getElementById("headlines-container")
+    const count = Array.from(headlinesContainer.children).length
+
     const headlineBox = document.createElement("div")
-    headlineBox.setAttribute("class", "headline-box")
+    headlineBox.setAttribute("class", "headline-box opacity-0")
+    headlineBox.style.transitionDelay = (3400 + 100 * count) + "ms"
     const textarea = document.createElement("textarea")
     headlineBox.appendChild(textarea)
     headlineBox.addEventListener("keyup", () => {
         updateTextareaHeight(textarea)
     })
 
-    const headlinesContainer = document.getElementById("headlines-container")
     headlinesContainer.appendChild(headlineBox)
 }
 
@@ -22,11 +25,27 @@ export function removeHeadline() {
 export function rewind() {
     const headImageContainer = document.getElementById("head-image-container")
     headImageContainer.setAttribute("class", "opacity-0")
+
+    const headlineBoxies = Array.from(document.getElementById("headlines-container").children)
+    headlineBoxies.forEach(element => {
+        const delay = element.style.transitionDelay
+        element.style.transitionDelay = "0ms"
+        element.setAttribute("class", "headline-box opacity-0")
+
+        window.setTimeout(() => {
+            element.style.transitionDelay = delay
+        }, 1)
+    });
 }
 
 export function start() {
     const headImageContainer = document.getElementById("head-image-container")
     headImageContainer.setAttribute("class", "fade-in")
+
+    const headlineBoxies = Array.from(document.getElementById("headlines-container").children)
+    headlineBoxies.forEach((element, index) => {
+        element.setAttribute("class", "headline-box headline-in")
+    });
 }
 
 function updateTextareaHeight(textarea) {
@@ -43,4 +62,8 @@ window.onload = () => {
     for (let index = 0; index < 4; index++) {
         addHeadline()
     }
+
+    window.setTimeout(() => {
+        start()
+    }, 10)
 }
