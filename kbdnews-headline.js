2ce79d12db3dcba0ad25411a8e5449dfcc4a9de1
Sano <sano@qoncept.co.jp>
2022-05-06 18:57:17 +0900

    Change headline focus method from hover to num key

--- kbdnews-headline.js
+++ kbdnews-headline.js
@@ -33,14 +33,15 @@ export function rewind() {
     const headImageContainer = document.getElementById("head-image-container")
     headImageContainer.setAttribute("class", "opacity-0")
 
-    const headlineBoxies = Array.from(document.getElementById("headlines-container").children)
-    headlineBoxies.forEach(element => {
-        const delay = element.style.transitionDelay
-        element.style.transitionDelay = "0ms"
-        element.setAttribute("class", "headline-out")
+    const headlineContainers = Array.from(document.getElementById("headlines-container").children)
+    headlineContainers.forEach(headlineContainer => {
+        const delay = headlineContainer.style.transitionDelay
+        headlineContainer.style.transitionDelay = "0ms"
+        headlineContainer.setAttribute("class", "headline-out")
+        headlineContainer.firstElementChild.setAttribute("class", "headline-box")
 
         window.setTimeout(() => {
-            element.style.transitionDelay = delay
+            headlineContainer.style.transitionDelay = delay
         }, 20)
     });
 }
@@ -49,8 +50,8 @@ export function start() {
     const headImageContainer = document.getElementById("head-image-container")
     headImageContainer.setAttribute("class", "fade-in")
 
-    const headlineBoxies = Array.from(document.getElementById("headlines-container").children)
-    headlineBoxies.forEach((element, index) => {
+    const headlineContainers = Array.from(document.getElementById("headlines-container").children)
+    headlineContainers.forEach((element) => {
         element.setAttribute("class", "headline-in")
     });
 }
@@ -74,3 +75,35 @@ window.onload = () => {
         start()
     }, 20)
 }
+
+document.addEventListener("keydown", (event) => {
+    const targetTag = event.target.tagName.toLowerCase()
+    if (targetTag == "input" || targetTag == "textarea") {
+        return
+    }
+
+    if ("0123456789".includes(event.key)) {
+        const index = parseInt(event.key) - 1
+
+        const headlineBoxies =
+            Array.from(document.getElementById("headlines-container").children)
+                .map((container) => { return container.lastElementChild })
+
+        if (0 <= index && index < headlineBoxies.length) {
+            const focusTarget = headlineBoxies[index]
+            const focused = focusTarget.classList.contains("headline-box-focus")
+
+            headlineBoxies.forEach((headlineBox) => {
+                headlineBox.setAttribute("class", "headline-box")
+            })
+
+            if (!focused) {
+                focusTarget.setAttribute("class", "headline-box headline-box-focus")
+            }
+        } else {
+            headlineBoxies.forEach((headlineBox) => {
+                headlineBox.setAttribute("class", "headline-box")
+            })
+        }
+    }
+})
